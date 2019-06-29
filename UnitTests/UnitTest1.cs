5bca4015ef1496d890329af1ba74c6df579eac56
Mike Koenig <mikekoenig@live.com>
2019-06-29 10:00:55 -0700

    Add project files.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliWeb.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+            var myTest = new ErrorViewModel();
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+    }
+}
