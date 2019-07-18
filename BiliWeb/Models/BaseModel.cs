39e77f99fccf32f6c9bdbf11767e753fef4940ae
Mike Koenig <mikekoenig@live.com>
2019-07-18 14:35:34 -0700

    Added Base Model for all data models

---
+++ BiliWeb/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    ///  Base Model all data models inherit from
+    ///  Contains the guid, and the data for the record
+    /// </summary>
+    public class BaseModel
+    {
+
+        // The ID for this data record as a guid
+        public string ID { get; set; } = System.Guid.NewGuid().ToString("D");
+
+        // When the record was created
+        public DateTime Date { get; set; } = System.DateTime.UtcNow;
+
+    }
+}
