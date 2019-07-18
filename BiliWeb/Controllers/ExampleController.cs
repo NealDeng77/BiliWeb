84a1387a83030eb44377b340f2319372a52b5fc7
Mike Koenig <mikekoenig@live.com>
2019-07-18 12:26:32 -0700

    Updated Example Controller for Comments Added Read Method

--- BiliWeb/Controllers/ExampleController.cs
+++ BiliWeb/Controllers/ExampleController.cs
@@ -9,11 +9,42 @@ namespace BiliWeb.Controllers
 {
     public class ExampleController : Controller
     {
+        /// <summary>
+        /// Show all the data
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Return the data for the ID passed in
+        /// </summary>
+        /// <returns>Data record, or redirects to error</returns>
+        [HttpGet]
+        public IActionResult Read(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            //TODO Look up the ID
+            var data = new ExampleModel();
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(data);
+        }
+
+        /// <summary>
+        /// Returns a blank create page with a pre-populated guid for the record
+        /// </summary>
+        /// <returns>new data model</returns>
         [HttpGet]
         public IActionResult Create()
         {
@@ -21,6 +52,11 @@ namespace BiliWeb.Controllers
             return View(data);
         }
 
+        /// <summary>
+        /// Receive a post for a new record
+        /// </summary>
+        /// <param name="data">The data to create</param>
+        /// <returns>Redirects to Index page</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(
