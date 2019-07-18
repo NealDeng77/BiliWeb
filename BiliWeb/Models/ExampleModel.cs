12afd043e8567a84fe7facc421c7f20104178a7e
Mike Koenig <mikekoenig@live.com>
2019-07-18 08:08:17 -0700

    Added Example Model to Project

---
+++ BiliWeb/Models/ExampleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    ///  Example Data Model
+    /// </summary>
+    public class ExampleModel
+    {
+        // The ID for this data record as a guid
+        public string ExampleID { get; set; } = new Guid().ToString();
+
+
+    }
+}
