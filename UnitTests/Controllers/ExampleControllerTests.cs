97f2c8a2bef91c78f451e5085842ec77187f1293
Mike Koenig <mikekoenig@live.com>
2019-07-18 13:51:26 -0700

    Added Update UT to Example

--- UnitTests/Controllers/ExampleControllerTests.cs
+++ UnitTests/Controllers/ExampleControllerTests.cs
@@ -25,6 +25,47 @@ namespace UnitTests.Controllers
             Assert.IsNotNull(myTest);
         }
 
+        #region UpdateTests
+        /// <summary>
+        /// Ensure the Update Method on the controller returns and is not null
+        /// </summary>
+        [TestMethod]
+        public void Example_Update_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myController = new ExampleController();
+
+            // Act
+            var myTest = myController.Update();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+
+        /// <summary>
+        /// Ensure the Update Method Post on the controller returns and is not null
+        /// </summary>
+        [TestMethod]
+        public void Example_Update_Post_Default_Should_Pass()
+        {
+            // Arrange
+            var myController = new ExampleController();
+            var myData = new ExampleModel();
+
+            // Act
+            var myTest = myController.Update(myData);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+        #endregion UpdateTests
+        
+        #region CreateTests
+
         /// <summary>
         /// Ensure the Create Method on the controller returns and is not null
         /// </summary>
@@ -61,7 +102,9 @@ namespace UnitTests.Controllers
             // Assert
             Assert.IsNotNull(myTest);
         }
+        #endregion CreateTests
 
+        #region ReadTests
         /// <summary>
         /// Ensure the Read Method with no data should fail
         /// </summary>
@@ -97,5 +140,44 @@ namespace UnitTests.Controllers
             // Assert
             Assert.IsNotNull(myTest);
         }
+        #endregion ReadTests
+
+        #region DeleteTests
+        /// <summary>
+        /// Ensure the Delete Method with no data should fail
+        /// </summary>
+        [TestMethod]
+        public void Example_Delete_Get_Data_Null_Should_Fail()
+        {
+            // Arrange
+            var myController = new ExampleController();
+
+            // Act
+            var myTest = myController.Delete(null);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+
+        /// <summary>
+        /// Ensure the Delete Method with no data should fail
+        /// </summary>
+        [TestMethod]
+        public void Example_Delete_Get_Data_In_Valid_Should_Fail()
+        {
+            // Arrange
+            var myController = new ExampleController();
+
+            // Act
+            var myTest = myController.Delete("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+        #endregion DeleteTests
     }
 }
