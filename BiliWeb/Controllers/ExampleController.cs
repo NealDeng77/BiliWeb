afb35235dcbb98c75d6844c3e951287a6a9969e5
Mike Koenig <mikekoenig@live.com>
2019-07-18 10:11:34 -0700

    Updated Example Controller for Create Get and Post.  Partial Checking, sill adding the Create cshtml

--- BiliWeb/Controllers/ExampleController.cs
+++ BiliWeb/Controllers/ExampleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BiliWeb.Models;
 
 namespace BiliWeb.Controllers
 {
@@ -11,6 +12,33 @@ namespace BiliWeb.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var data = new ExampleModel();
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("" +
+            "ExampleID,"+
+            "Name,"+
+            "")] ExampleModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            // Todo Save Change
+            //data.ExampleID;
+            //await SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
