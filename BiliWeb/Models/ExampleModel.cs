b90a5a90f2ce1989e24d0d09d491e14831654dbd
Mike Koenig <mikekoenig@live.com>
2019-07-18 14:50:27 -0700

    Added Update method to example Model

--- BiliWeb/Models/ExampleModel.cs
+++ BiliWeb/Models/ExampleModel.cs
@@ -14,5 +14,31 @@ namespace BiliWeb.Models
         // Just a field to show how to use it...
         public string Name { get; set; }
 
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// Does NOT update the Date field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(ExampleModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old one in place
+            // ID = data.ID;
+
+            // Don't update the Date, leave the old one in place
+            // Date = data.Date;
+
+            // Update all the other fields
+            Name= data.Name;
+
+            return true;
+        }
     }
 }
