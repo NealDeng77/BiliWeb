3d403941e0fdfca93efa842a6d44b30bec6ac433
Mike Koenig <mikekoenig@live.com>
2019-07-18 14:35:47 -0700

    Refacrtored ExampleModel to use Base Model

--- BiliWeb/Models/ExampleModel.cs
+++ BiliWeb/Models/ExampleModel.cs
@@ -8,10 +8,8 @@ namespace BiliWeb.Models
     /// <summary>
     ///  Example Data Model
     /// </summary>
-    public class ExampleModel
+    public class ExampleModel : BaseModel
     {
-        // The ID for this data record as a guid
-        public string ExampleID { get; set; } = System.Guid.NewGuid().ToString("D");
 
         // Just a field to show how to use it...
         public string Name { get; set; }
