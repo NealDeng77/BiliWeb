cb3ea1b9c21febcb80a54f7ca77f03507a753279
Mike Koenig <mikekoenig@live.com>
2019-07-19 11:06:22 -0700

    Added ResultLogModel and UT

--- BiliWeb/Models/ResultLogModel.cs
+++ BiliWeb/Models/ResultLogModel.cs
@@ -12,27 +12,70 @@ namespace BiliWeb.Models
     /// 
     /// The Photo is passed in a separate call based on the guid created
     /// </summary>
-    public class ResultLogModel
+    public class ResultLogModel : BaseModel
     {
-        // The Guid for this Record
-        public string ResultLogID { get; set; } = new Guid().ToString();
+        //// The Bilirubin result Value
+        //public double BilirubinValue { get; set; }
 
-        // The Date Time in UTC when this record was created
-        public DateTime ResultLogDateTime { get; set; } = DateTime.UtcNow;
+        //// The guid to the Clinic where the value was recorded
+        //public string ClinicID { get; set; }
 
-        // The Bilirubin result Value
-        public double BilirubinValue { get; set; }
+        //// The guid to the Phone that took the picture
+        //public string PhoneID { get; set; }
 
-        // The guid to the Clinic where the value was recorded
-        public string ClinicID { get; set; }
+        //// The guid to the User who took the pictures
+        //public string UserID { get; set; }
 
-        // The guid to the Phone that took the picture
-        public string PhoneID { get; set; }
+        //// The guid to the photo set for this record, created here and passed back to the phone, and thus used for storing the photos
+        //public string PhotoID { get; set; } = new Guid().ToString();
 
-        // The guid to the User who took the pictures
-        public string UserID { get; set; }
 
-        // The guid to the photo set for this record, created here and passed back to the phone, and thus used for storing the photos
-        public string PhotoID { get; set; } = new Guid().ToString();
+        // Just a field to show how to use it...
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Simple Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        public ResultLogModel()
+        {
+        }
+
+        /// <summary>
+        /// Makes a copy of the data
+        /// </summary>
+        /// <param name="data"></param>
+        public ResultLogModel(ResultLogModel data)
+        {
+            // Because this is a copy, let it have a new ID
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// Does NOT update the Date field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(ResultLogModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old one in place
+            // ID = data.ID;
+
+            // Don't update the Date, leave the old one in place
+            // Date = data.Date;
+
+            // Update all the other fields
+            Name = data.Name;
+
+            return true;
+        }
     }
 }
---
+++ UnitTests/Models/ResultLogModelTests.cs
@@ -0,0 +1,111 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliWeb.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ResultLogModelTests
+    {
+        /// <summary>
+        /// Stand up a new Model, make sure it is not null
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var myTest = new ResultLogModel();
+
+            // Assert
+            Assert.IsNotNull(myTest);
+        }
+
+        /// <summary>
+        /// Instantiate Model with Data
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Constructor_Data_Valid_Should_Pass()
+        {
+            // Arrange
+            var myData = new ResultLogModel
+            {
+                Name = "New"
+            };
+
+            // Act
+            var myNewData = new ResultLogModel(myData);
+
+            // Assert
+            Assert.AreEqual("New", myNewData.Name);
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Update_InValid_Data_Null_Should_Fail()
+        {
+            // Arrange
+            var myData = new ResultLogModel();
+
+            // Act
+            var result = myData.Update(null);
+
+            // Assert
+            Assert.AreEqual(false,result);
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Update_Valid_Data_Good_Should_Pass()
+        {
+            // Arrange
+            var myData = new ResultLogModel();
+            var myDataNew = new ResultLogModel
+            {
+                Name = "New",
+                ID = myData.ID
+            };
+
+            // Act
+            myData.Update(myDataNew);
+
+            // Assert
+            Assert.AreEqual("New", myData.Name);
+        }
+
+        /// <summary>
+        /// Get test for Model
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Get_Should_Pass()
+        {
+            // Arrange
+            var myData = new ResultLogModel();
+
+            // Act
+
+            // Assert
+            Assert.IsNull(myData.Name);
+        }
+
+        /// <summary>
+        /// Set test for Model
+        /// </summary>
+        [TestMethod]
+        public void ResultLog_Set_Should_Pass()
+        {
+            // Arrange
+            var myData = new ResultLogModel();
+
+            // Act
+            myData.Name = "New";
+
+            // Assert
+            Assert.AreEqual("New", myData.Name);
+        }
+    }
+}
