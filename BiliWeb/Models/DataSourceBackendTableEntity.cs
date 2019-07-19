a3cde888405a1057dc592b53a0c9a24f0950d35a
Mike Koenig <mikekoenig@live.com>
2019-07-18 18:02:45 -0700

    Added DataSourceBackendTable Entity

---
+++ BiliWeb/Models/DataSourceBackendTableEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    /// This Converts a Table Entity to a Json Format for Retreival
+    /// Used to Store the Data into the Azure Table Storage which is a Key Value Pair Store.
+    /// </summary>
+    public class DataSourceBackendTableEntity : TableEntity
+    {
+
+        /// <summary>
+        /// data blob to store
+        /// A Json format of the desired data package
+        /// Needs to be converted to Json and passed in
+        /// Results, need to be convered from TableEntity and parsed 
+        /// </summary>
+        public string Blob { get; set; }
+
+        public DataSourceBackendTableEntity(string pk, string rk, string blob)
+            : base(pk, rk)
+        {
+            Blob = blob;
+        }
+
+        public DataSourceBackendTableEntity(string pk, string rk)
+            : base(pk, rk) { }
+
+        public DataSourceBackendTableEntity() { }
+    }
+}
