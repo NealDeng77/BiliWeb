ac5ee140ecb371994560e32852944740dd9ee575
Mike Koenig <mikekoenig@live.com>
2019-07-19 10:40:48 -0700

    Updated Comments on Example Repository Interface

--- BiliWeb/Backend/Example/IExampleRepository.cs
+++ BiliWeb/Backend/Example/IExampleRepository.cs
@@ -10,19 +10,35 @@ namespace BiliWeb.Backend
     /// <summary>
     /// Define an interface which contains the methods for the Example repository.
     /// All CRUDi aspects
+    /// 
     /// </summary>
     public interface IExampleRepository
     {
+        // Adds a new record to the data source
         ExampleModel Create(ExampleModel data, DataSourceEnum dataSourceEnum = DataSourceEnum.Unknown);
+
+        // Returns the record of the matching ID or null if not found
         ExampleModel Read(String id);
+
+        // Update the record passed in,  the ID and Date are not modified
         ExampleModel Update(ExampleModel data);
+
+        // Delete the record with the id passed in, datasource is used for Store calls
         Boolean Delete(String id, DataSourceEnum dataSourceEnum = DataSourceEnum.Unknown);
+
+        // Return all items
         List<ExampleModel> Index();
 
+        /// Reset, clears the current data and loads fresh
         void Reset();
-        void LoadDataSet(DataSourceDataSetEnum setEnum);
-        bool BackupData(DataSourceEnum dataSourceSource, DataSourceEnum dataSourceDestination);
-        string GetDataSourceString();
 
+        /// LoadDataSet specifies which data to load
+        void LoadDataSet(DataSourceDataSetEnum setEnum);
+
+        // BakcupData copies data from one source to another
+        bool BackupData(DataSourceEnum dataSourceSource, DataSourceEnum dataSourceDestination);
+
+        // GetDataSourceString returns the repository type name, either Mock, or Store
+        string GetDataSourceString();
     }
 }
