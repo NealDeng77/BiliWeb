801705e933cc4ffcbbcb4395e282861e58717d75
Mike Koenig <mikekoenig@live.com>
2019-07-19 18:56:03 -0700

    Added a ResultsLogHelper to create the SelectedList for the Result IDs

---
+++ BiliWeb/Backend/ResultLog/ResultLogHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BiliWeb.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BiliWeb.Backend
+{
+    public static class ResultLogHelper
+    {
+        /// <summary>
+        /// Convert the list of data to a Select List
+        /// This allows it to be used in a Drop Down List Box
+        /// Update Value to be the Value to show in the Box
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<SelectListItem> ToSelectListItems(this IEnumerable<ResultLogModel> dataSet, string selectedId)
+        {
+            return
+                dataSet.OrderBy(m => m.ID)
+                      .Select(m =>
+                          new SelectListItem
+                          {
+                              Selected = (m.ID == selectedId),
+                              Value = m.ID.ToString(),
+
+                              Text = m.BilirubinValue.ToString()
+                          });
+        }
+    }
+}
