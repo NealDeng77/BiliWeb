7ef6dfd221efa2ef21cc257581524d3002051226
Mike Koenig <mikekoenig@live.com>
2019-07-20 22:49:57 -0700

    Added Coverage for ResultLogHelper ToSelectListItems
    ResultLogHelper now 100%

--- UnitTests/Backend/ResultLog/ResultLogHelperTests.cs
+++ UnitTests/Backend/ResultLog/ResultLogHelperTests.cs
@@ -1,12 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BiliWeb.Models;
 using BiliWeb.Backend;
+using System.Linq;
 
 namespace UnitTests.Backend
 {
     [TestClass]
     public class ResultLogHelperTests
     {
+
+        #region ConvertIDtoStringTests
         /// <summary>
         /// Convert Valid ID to Value
         /// </summary>
@@ -27,7 +30,7 @@ namespace UnitTests.Backend
             DataSourceBackend.Instance.Reset();
 
             // Assert
-            Assert.AreEqual("55",result);
+            Assert.AreEqual("55", result);
         }
 
         /// <summary>
@@ -44,7 +47,7 @@ namespace UnitTests.Backend
             // Reset
 
             // Assert
-            Assert.AreEqual(string.Empty,result);
+            Assert.AreEqual(string.Empty, result);
         }
 
         /// <summary>
@@ -63,5 +66,81 @@ namespace UnitTests.Backend
             // Assert
             Assert.AreEqual(string.Empty, result);
         }
+        #endregion ConvertIDtoStringTests    
+
+        #region ToSelectListItemsTests
+        /// <summary>
+        /// Returns a set of Select List Items from the data set of ResultLog
+        /// Used for converting the records to a list that can go into a drop down list box
+        /// </summary>
+        [TestMethod]
+        public void ResultLogHelper_ToSelectListItemsTests_Valid_Should_Pass()
+        {
+            // Arrange
+            var data = DataSourceBackend.Instance.ResultLogBackend.Index();
+
+            // Act
+            var result = ResultLogHelper.ToSelectListItems(data,null);
+
+            // Reset
+
+            // Assert
+            // Check each item returned, and make sure it matches the original data
+            foreach (var item in result)
+            {
+                Assert.AreEqual(item.Text, data.Find(m=>m.ID==item.Value).BilirubinValue.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Returns a set of Select List Items from the data set of ResultLog
+        /// Make sure the Selected Value is returned
+        /// </summary>
+        [TestMethod]
+        public void ResultLogHelper_ToSelectListItemsTests_Valid_Selected_Should_Pass()
+        {
+            // Arrange
+            var data = DataSourceBackend.Instance.ResultLogBackend.Index();
+
+            // Choose item to be selected
+            var value = data[0].ID;
+
+            // Act
+            var result = ResultLogHelper.ToSelectListItems(data, value);
+
+            // Reset
+
+            // Assert
+            // The First should be the seleted item.
+            Assert.AreEqual(result.First(m => m.Selected == true).Value, data.Find(m => m.ID == value).ID);
+            // Only One should be Selected
+            Assert.AreEqual(1, result.Where(m => m.Selected == true).Count());
+
+        }
+
+        /// <summary>
+        /// Returns a set of Select List Items from the data set of ResultLog
+        /// If the Selected item is not in the list, return the list with no selected items
+        /// </summary>
+        [TestMethod]
+        public void ResultLogHelper_ToSelectListItemsTests_InValid_Selected_Should_Pass()
+        {
+            // Arrange
+            var data = DataSourceBackend.Instance.ResultLogBackend.Index();
+
+            // Choose item to be selected
+            var value = "bogus";
+
+            // Act
+            var result = ResultLogHelper.ToSelectListItems(data, value);
+            var resultCount = result.Where(m => m.Selected == true).Count();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, resultCount);
+        }
+        #endregion ToSelectListItemsTests
     }
 }
