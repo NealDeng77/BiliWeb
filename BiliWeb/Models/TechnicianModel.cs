c5ca94c1bb819fbcdc6a7b0738f2be101578ca52
Laura Larson <larsonlaura@seattleu.edu>
2019-07-21 18:46:54 -0700

    Added TechnicianModel and TechnicianModelTests classes

---
+++ BiliWeb/Models/TechnicianModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    ///  Example Data Model
+    /// </summary>
+    public class TechnicianModel : BaseModel
+    {
+
+        // Just a field to show how to use it...
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Simple Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        public TechnicianModel()
+        {
+        }
+
+        /// <summary>
+        /// Makes a copy of the data
+        /// </summary>
+        /// <param name="data"></param>
+        public TechnicianModel(TechnicianModel data)
+        {
+            // Because this is a copy, let it have a new ID
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// Does NOT update the Date field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(TechnicianModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old one in place
+            // ID = data.ID;
+
+            // Don't update the Date, leave the old one in place
+            // Date = data.Date;
+
+            // Update all the other fields
+            Name= data.Name;
+
+            return true;
+        }
+    }
+}
