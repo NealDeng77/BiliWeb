41a740572b07a1a1ccc7d15f6ffe70d216cb2f64
Carrie <clipsong@gmail.com>
2019-07-22 15:35:38 -0700

    Added Inventory Model and Inventory Unit tests

---
+++ BiliWeb/Models/InventoryModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    ///  Inventory Model
+    /// </summary>
+    public class InventoryModel : BaseModel
+    {
+
+        // The guid to the Clinic where the value was recorded
+        public string ClinicID { get; set; }
+
+        // The int count of current test strips in stock
+        public int TestStripStock { get; set; }
+
+
+        /// <summary>
+        /// Simple Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        public InventoryModel()
+        {
+        }
+
+        /// <summary>
+        /// Makes a copy of the data
+        /// </summary>
+        /// <param name="data"></param>
+        public InventoryModel(InventoryModel data)
+        {
+            // Because this is a copy, let it have a new ID
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// Does NOT update the Date field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(InventoryModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old one in place
+            // ID = data.ID;
+
+            // Don't update the Date, leave the old one in place
+            // Date = data.Date;
+
+            // Update all the other fields
+            ClinicID = data.ClinicID;
+            TestStripStock = data.TestStripStock;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTests/Models/InventoryModelTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliWeb.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class InventoryModelTests
+    {
+        /// <summary>
+        /// Stand up a new Model, make sure it is not null
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new InventoryModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        /// <summary>
+        /// Instantiate Model with Data
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Constructor_Data_Valid_Should_Pass()
+        {
+            // Arrange
+            var myData = new InventoryModel
+            {
+                ClinicID = "Clinic",
+                TestStripStock = 50
+            };
+        
+
+            // Act
+            var myNewData = new InventoryModel(myData);
+
+            // Assert clinic 
+            Assert.AreEqual("Clinic", myNewData.ClinicID);
+
+            //Assert test strip count
+            Assert.AreEqual(50, myNewData.TestStripStock);
+
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Update_InValid_Data_Null_Should_Fail()
+        {
+            // Arrange
+            var myData = new InventoryModel();
+
+            // Act
+            var result = myData.Update(null);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Update_Valid_Data_Good_Should_Pass()
+        {
+            // Arrange
+            var myData = new InventoryModel();
+            var myDataNew = new InventoryModel
+            {
+                //Clinic 
+                ClinicID = "Clinic",
+
+                //Count of test strips
+                TestStripStock = 50,
+
+                ID = myData.ID
+            };
+
+            // Act
+            myData.Update(myDataNew);
+
+            // Assert
+            // Clinic and test strip count attribute should change
+            Assert.AreEqual("Clinic", myData.ClinicID);
+            Assert.AreEqual(50, myData.TestStripStock);
+
+            Assert.AreNotEqual(myData.Date, myDataNew.Date);
+
+        }
+
+        /// <summary>
+        /// Get test for Model
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Get_Should_Pass()
+        {
+            // Arrange
+            var myData = new InventoryModel();
+
+            // Act
+
+            // Assert
+            Assert.IsNull(myData.ClinicID);
+            Assert.AreEqual(0, myData.TestStripStock);
+
+        }
+
+        /// <summary>
+        /// Set test for Model
+        /// </summary>
+        [TestMethod]
+        public void Inventory_Set_Should_Pass()
+        {
+            // Arrange
+            var myData = new InventoryModel();
+
+            // Act
+            myData.ClinicID = "Clinic";
+            myData.TestStripStock = 12;
+
+            // Assert
+            Assert.AreEqual("Clinic", myData.ClinicID);
+            Assert.AreEqual(12, myData.TestStripStock);
+
+        }
+    }
+}
