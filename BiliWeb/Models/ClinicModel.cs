4a00a4b0c8d3221f80eb25e0a5a599971d9b2e15
heetaey <htyang90@gmail.com>
2019-07-22 19:58:27 -0700

    Added ClinicModel and ClinicModelTests

---
+++ BiliWeb/Models/ClinicModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiliWeb.Models
+{
+    /// <summary>
+    ///  Example Data Model
+    /// </summary>
+    public class ClinicModel : BaseModel
+    {
+
+        // Name of the clinic
+        public string Name { get; set; }
+
+        // Address of the clinic
+        public string Address { get; set; }
+
+        // City of the clinic
+        public string City { get; set; }
+
+        // Country of the clinic
+        public string Country { get; set; }
+
+        // Contact person of the clinic
+        public string Contact { get; set; }
+
+        // Phone number of the clinic
+        public string Phone { get; set; }
+
+        // Email of the clinic
+        public string Email { get; set; }
+
+        // Notes of the clinic
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Simple Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        public ClinicModel()
+        {
+        }
+
+        /// <summary>
+        /// Makes a copy of the data
+        /// </summary>
+        /// <param name="data"></param>
+        public ClinicModel(ClinicModel data)
+        {
+            // Because this is a copy, let it have a new ID
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// Does NOT update the Date field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(ClinicModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old one in place
+            // ID = data.ID;
+
+            // Don't update the Date, leave the old one in place
+            // Date = data.Date;
+
+            // Update all the other fields
+            Name = data.Name;
+            Address = data.Address;
+            City = data.City;
+            Country = data.Country;
+            Contact = data.Contact;
+            Phone = data.Phone;
+            Email = data.Email;
+            Notes = data.Notes;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTests/Models/ClinicModelTests.cs
@@ -0,0 +1,165 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliWeb.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ClinicModelTests
+    {
+        /// <summary>
+        /// Stand up a new Model, make sure it is not null
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ClinicModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        /// <summary>
+        /// Instantiate Model with Data
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Constructor_Data_Valid_Should_Pass()
+        {
+            // Arrange
+            var myData = new ClinicModel
+            {
+                Name = "Bellevue Hospital",
+                Address = "13 Bellevue Dr.",
+                City = "Bellevue",
+                Country = "USA",
+                Contact = "John Appleseed",
+                Phone = "+14254254252",
+                Email = "jappleseed@gmail.com",
+                Notes = "Newly opened"
+            };
+
+            // Act
+            var myNewData = new ClinicModel(myData);
+
+            // Assert
+            Assert.AreEqual("Bellevue Hospital", myNewData.Name);
+            Assert.AreEqual("13 Bellevue Dr.", myNewData.Address);
+            Assert.AreEqual("Bellevue", myNewData.City);
+            Assert.AreEqual("USA", myNewData.Country);
+            Assert.AreEqual("John Appleseed", myNewData.Contact);
+            Assert.AreEqual("+14254254252", myNewData.Phone);
+            Assert.AreEqual("jappleseed@gmail.com", myNewData.Email);
+            Assert.AreEqual("Newly opened", myNewData.Notes);
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Update_InValid_Data_Null_Should_Fail()
+        {
+            // Arrange
+            var myData = new ClinicModel();
+
+            // Act
+            var result = myData.Update(null);
+
+            // Assert
+            Assert.AreEqual(false,result);
+        }
+
+        /// <summary>
+        /// Update Model with bogus data should Fail
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Update_Valid_Data_Good_Should_Pass()
+        {
+            // Arrange
+            var myData = new ClinicModel();
+            var myDataNew = new ClinicModel
+            {
+                Name = "Bellevue Hospital",
+                Address = "13 Bellevue Dr.",
+                City = "Bellevue",
+                Country = "USA",
+                Contact = "John Appleseed",
+                Phone = "+14254254252",
+                Email = "jappleseed@gmail.com",
+                Notes = "Newly opened",
+                ID = myData.ID
+            };
+
+            // Act
+            myData.Update(myDataNew);
+
+            // Assert
+            Assert.AreEqual("Bellevue Hospital", myData.Name);
+            Assert.AreEqual("13 Bellevue Dr.", myData.Address);
+            Assert.AreEqual("Bellevue", myData.City);
+            Assert.AreEqual("USA", myData.Country);
+            Assert.AreEqual("John Appleseed", myData.Contact);
+            Assert.AreEqual("+14254254252", myData.Phone);
+            Assert.AreEqual("jappleseed@gmail.com", myData.Email);
+            Assert.AreEqual("Newly opened", myData.Notes);
+
+            Assert.AreNotEqual(myData.Date, myDataNew.Date);
+        }
+
+        /// <summary>
+        /// Get test for Model
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Get_Should_Pass()
+        {
+            // Arrange
+            var myData = new ClinicModel();
+
+            // Act
+
+            // Assert
+            Assert.IsNull(myData.Name);
+            Assert.IsNull(myData.Address);
+            Assert.IsNull(myData.City);
+            Assert.IsNull(myData.Country);
+            Assert.IsNull(myData.Contact);
+            Assert.IsNull(myData.Phone);
+            Assert.IsNull(myData.Email);
+            Assert.IsNull(myData.Notes);
+
+        }
+
+        /// <summary>
+        /// Set test for Model
+        /// </summary>
+        [TestMethod]
+        public void Clinic_Set_Should_Pass()
+        {
+            // Arrange
+            var myData = new ClinicModel();
+
+            // Act
+            myData.Name = "Bellevue Hospital";
+            myData.Address = "13 Bellevue Dr.";
+            myData.City = "Bellevue";
+            myData.Country = "USA";
+            myData.Contact = "John Appleseed";
+            myData.Phone = "+14254254252";
+            myData.Email = "jappleseed@gmail.com";
+            myData.Notes = "Newly opened";
+
+            // Assert
+            Assert.AreEqual("Bellevue Hospital", myData.Name);
+            Assert.AreEqual("13 Bellevue Dr.", myData.Address);
+            Assert.AreEqual("Bellevue", myData.City);
+            Assert.AreEqual("USA", myData.Country);
+            Assert.AreEqual("John Appleseed", myData.Contact);
+            Assert.AreEqual("+14254254252", myData.Phone);
+            Assert.AreEqual("jappleseed@gmail.com", myData.Email);
+            Assert.AreEqual("Newly opened", myData.Notes);
+
+            // TODO:  Add an Assert for each attribute
+        }
+    }
+}
