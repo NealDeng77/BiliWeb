367eb9b731ce810f3354c1c7db08467c95fbc6ff
Mike Koenig <koenigm@seattleu.edu>
2019-07-25 13:28:08 -0700

    Updated UT for API, Moved them to an API Folder

---
+++ UnitTests/Models/API/APIBaseModelTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliWeb.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class APIBaseModelTests
+    {
+        /// <summary>
+        /// Stand up a new Model, make sure it is not null
+        /// </summary>
+        [TestMethod]
+        public void APIBaseModel_InValid_Empty_Phone_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = new APIBaseModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
