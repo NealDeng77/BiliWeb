c690c7b9a94016bc840b524b0c8cb437861ed240
Mike Koenig <koenigm@seattleu.edu>
2019-07-25 14:04:07 -0700

    Increase UT Coverage for Phone Settings Model

--- UnitTests/Models/API/PhoneSettingsModelTests.cs
+++ UnitTests/Models/API/PhoneSettingsModelTests.cs
@@ -1,7 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BiliWeb.Models;
+using System.Collections.Generic;
 
-namespace UnitTests.Models
+namespace UnitTests.Models.API
 {
     [TestClass]
     public class PhoneSettingsModelTests
@@ -21,6 +22,59 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
-        
+
+        /// <summary>
+        /// Set test for PhoneSettings Model
+        /// </summary>
+        [TestMethod]
+        public void PhoneSettings_Set_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new PhoneSettingsModel
+            {
+                Status = 10,
+                Message = "Message",
+                PhoneModel = new PhoneModel(),
+                UserList = new List<TechnicianModel>()
+            };
+
+            // Assert
+            Assert.AreEqual(10, result.Status);
+            Assert.AreEqual("Message", result.Message);
+            Assert.IsNotNull(result.PhoneModel);
+            Assert.IsNotNull(result.UserList);
+        }
+
+        /// <summary>
+        /// Set test for PhoneSettings Model
+        /// </summary>
+        [TestMethod]
+        public void PhoneSettings_Get_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new PhoneSettingsModel
+            {
+                Status = 10,
+                Message = "Message",
+                PhoneModel = new PhoneModel(),
+                UserList = new List<TechnicianModel>()
+            };
+
+            var Status = result.Status;
+            var Message = result.Message;
+            var PhoneModel = result.PhoneModel;
+            var UserList = result.UserList;
+
+            // Assert
+            Assert.AreEqual(10, Status);
+            Assert.AreEqual("Message", Message);
+            Assert.IsNotNull(PhoneModel);
+            Assert.IsNotNull(UserList);
+        }
+
     }
 }
