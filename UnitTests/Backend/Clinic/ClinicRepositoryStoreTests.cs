5b8fa04edd276d49523064a337fbbce8b97502b6
Mike Koenig <koenigm@seattleu.edu>
2019-07-29 21:15:00 -0700

    Hooked up Switching DataSource to Local, and using Local Reset to ClientRepositoryStoreTests

--- UnitTests/Backend/Clinic/ClinicRepositoryStoreTests.cs
+++ UnitTests/Backend/Clinic/ClinicRepositoryStoreTests.cs
@@ -17,7 +17,8 @@ namespace UnitTests.Backend
         public void Clinic_Index_Get_Default_Should_Pass()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Index();
@@ -37,14 +38,16 @@ namespace UnitTests.Backend
         public void Clinic_Create_Default_Should_Pass()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
             var myData = new ClinicModel();
 
             // Act
             var result = myBackend.Create(myData);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNotNull(result);
@@ -57,14 +60,16 @@ namespace UnitTests.Backend
         public void Clinic_Create_InValid_Null_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
             var myData = new ClinicModel();
 
             // Act
             var result = myBackend.Create(null);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNull(result);
@@ -79,12 +84,15 @@ namespace UnitTests.Backend
         public void Clinic_Read_Get_Data_InValid_Null_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Read(null);
 
             // Reset
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNull(result);
@@ -97,12 +105,15 @@ namespace UnitTests.Backend
         public void Clinic_Read_Get_Data_InValid_Bogus_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Read("bogus");
 
             // Reset
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNull(result);
@@ -117,7 +128,8 @@ namespace UnitTests.Backend
         public void Clinic_Update_Default_Should_Pass()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
             var myData = myBackend.Index().FirstOrDefault();
 
             // Make a Copy of the Data and update an aspect of it
@@ -129,7 +141,8 @@ namespace UnitTests.Backend
             var result = myBackend.Update(myDataCopy);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.AreEqual("New3", result.Name);
@@ -143,13 +156,15 @@ namespace UnitTests.Backend
         public void Clinic_Update_InValid_Null_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Update(null);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.AreEqual(null, result);
@@ -162,7 +177,8 @@ namespace UnitTests.Backend
         public void Clinic_Update_InValid_Bogus_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
             var myDataCopy = new ClinicModel
             {
                 ID = "bogus"
@@ -172,7 +188,8 @@ namespace UnitTests.Backend
             var result = myBackend.Update(myDataCopy);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.AreEqual(null, result);
@@ -187,12 +204,15 @@ namespace UnitTests.Backend
         public void Clinic_Delete_InValid_Data_Null_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Delete(null);
 
             // Reset
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNotNull(result);
@@ -205,12 +225,15 @@ namespace UnitTests.Backend
         public void Clinic_Delete_InValid_Data_Bogus_Should_Fail()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.Delete("bogus");
 
             // Reset
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.IsNotNull(result);
@@ -229,14 +252,16 @@ namespace UnitTests.Backend
         public void Clinic_Reset_Data_Valid_Should_Pass()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
             var dataOriginal = myBackend.Index().FirstOrDefault();
 
             // Act
             myBackend.Delete(dataOriginal.ID);
 
             // Reset
-            myBackend.Reset();
+            DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.AreEqual(dataOriginal.Name, myBackend.Index().FirstOrDefault().Name);
@@ -247,52 +272,6 @@ namespace UnitTests.Backend
         // Add Later, after Table is working and Moq is enabled
         #endregion BackupDataTests
 
-        #region Set_DataSetTests
-        /// <summary>
-        /// Call for The Demo Data Set
-        /// Then reset to the Default
-        /// Return True, because no different currently
-        /// If different sets are implemented, then verify the sets
-        /// </summary>
-        [TestMethod]
-        public void Clinic_DataSetDemo_Data_Valid_Should_Pass()
-        {
-            // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
-
-            // Act
-            myBackend.LoadDataSet(DataSourceDataSetEnum.Demo);
-
-            // Reset
-            myBackend.LoadDataSet(DataSourceDataSetEnum.Default);
-
-            // Assert
-            Assert.IsTrue(true);
-        }
-
-        /// <summary>
-        /// Call for The Demo Data Unit Test Set
-        /// Then reset to the Default
-        /// Return True, because no different currently
-        /// If different sets are implemented, then verify the sets
-        /// </summary>
-        [TestMethod]
-        public void Clinic_DataSetUnitTest_Data_Valid_Should_Pass()
-        {
-            // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
-
-            // Act
-            myBackend.LoadDataSet(DataSourceDataSetEnum.UnitTest);
-
-            // Reset
-            myBackend.LoadDataSet(DataSourceDataSetEnum.Default);
-
-            // Assert
-            Assert.IsTrue(true);
-        }
-        #endregion Set_DataSetTests
-
         #region GetDataSourceStringTests
         /// <summary>
         /// String should Match the Store or Store
@@ -301,13 +280,15 @@ namespace UnitTests.Backend
         public void Clinic_GetDataSourceString_Data_Valid_Should_Pass()
         {
             // Arrange
-            var myBackend = ClinicRepositoryStore.Instance;
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Local);
+            var myBackend = DataSourceBackend.Instance.ClinicBackend;
 
             // Act
             var result = myBackend.GetDataSourceString();
 
             // Reset
-            myBackend.LoadDataSet(DataSourceDataSetEnum.Default);
+           DataSourceBackend.Instance.Reset();
+            DataSourceBackend.Instance.SetDataSource(DataSourceEnum.Mock);
 
             // Assert
             Assert.AreEqual("Store", result);
