a34c30f12416dc9569db85eb0a8a37bdb38dfe24
Alex Peterson <petersonalex@seattleu.edu>
2019-08-04 22:40:31 -0700

    Added comments to PhoneReportViewModel class.

--- BiliWeb/Models/PhoneReportViewModel.cs
+++ BiliWeb/Models/PhoneReportViewModel.cs
@@ -5,25 +5,46 @@ using System.Threading.Tasks;
 
 namespace BiliWeb.Models
 {
+    /// <summary>
+    /// PhoneReportViewModel class
+    /// </summary>
     public class PhoneReportViewModel
     {
+        /// <summary>
+        /// The PhoneModel for this PhoneReportViewModel
+        /// </summary>
         public PhoneModel PhoneModel { get; set; }
 
+        /// <summary>
+        /// The current operating system installed on the phone
+        /// </summary>
         public VersionOSModel CurrentOSVersion { get; set; }
 
+        /// <summary>
+        /// The current app version installed on the phone
+        /// </summary>
         public VersionAppModel CurrentAppVersion { get; set; }
 
-        // The list of HistoryOS records for this phone
+        /// <summary>
+        /// The phone's operating system history. 
+        /// </summary>
         public List<HistoryOSModel> PhoneOSHistory = new List<HistoryOSModel>();
 
-        // The list of HistoryApp records for this phone
-        public List<HistoryAppModel> PhoneAppHistory = new List<HistoryAppModel>(); 
+        /// <summary>
+        /// The phone's application version history
+        /// </summary>
+        public List<HistoryAppModel> PhoneAppHistory = new List<HistoryAppModel>();
 
+        /// <summary>
+        /// The date that the application was initially installed on this phone
+        /// </summary>
         public DateTime InitialInstall { get; set; }
 
-        // The last time this phone was heard from in any capacity 
+        /// <summary>
+        /// The last time that this phone was heard from in any capacity 
+        /// </summary>
         public DateTime LastHeardFrom { get; set; }
 
-        // add more attributes here as needed 
+        // TODO: add more attributes here as needed 
     }
 }
